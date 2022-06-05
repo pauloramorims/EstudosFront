3ceee1599e817e783c936d5dd6af0a295a1a17cd
Paulo Ricardo Amorim <pauloramorims@gmail.com>
2022-06-05 20:53:30 -0300

    inserindo numeros no input apartir de um evento de teclado aplicando regex em algumas teclas

--- JavaScript/Telefone/main.js
+++ JavaScript/Telefone/main.js
@@ -1,13 +1,34 @@
 const tela = document.querySelector('.tela')
 const listaDeTeclas = document.querySelectorAll('.tecla');
+const teclado = document.querySelector('.teclado')
 
 function escreveNaTela(valor){
-    console.log(valor)
-    tela.value += valor
+    if(valor === 'Backspace' || valor === 'Delete'){
+        tela.value = tela.value.slice(0, - 1)
+    } else{
+        tela.value += valor
+    }
 }
 
 for (let index = 0; index < listaDeTeclas.length; index++) {
-    listaDeTeclas[index].addEventListener('click', ()=> {
+    const tecla = listaDeTeclas[index]
+    tecla.addEventListener('click', ()=> {
         escreveNaTela(listaDeTeclas[index].value)
     })
+
+}
+
+teclado.onkeydown = (e) => {
+    const teclasPermitidas = [/Backspace/, /Digit/, /Numpad/, /Delete/]
+
+    const testeRegex = teclasPermitidas.map(teclaPermitida => {
+        return e.code.match(teclaPermitida)
+    })
+    
+    for (let index = 0; index < testeRegex.length; index++) {
+        if(testeRegex[index] !== null) {
+            escreveNaTela(e.key)
+            break
+        }
+    }
 }
--- JavaScript/aluramidi/main.js
+++ JavaScript/aluramidi/main.js
@@ -1,1 +1,27 @@
-document.querySelector('.tecla_pom');
+
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = (event) => {
+       if (event.code === 'Space' || event.code === 'Enter'){
+           tecla.classList.add('ativa')
+       }
+    }
+    tecla.onkeyup = () => {
+       tecla.classList.remove('ativa')
+    }
+}
